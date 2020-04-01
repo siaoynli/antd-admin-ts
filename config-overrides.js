4517c73db2f34b70b7dbc72eb26cac772e217bb3
siaoynli <siaoynli@126.com>
2020-04-01 16:14:09 +0800

    ok

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: lixiaoyun
+ * @Company: http://hangzhou.com.cn
+ * @Github: http://github.com/siaoynli
+ * @Date: 2020-04-01 15:39:37
+ * @Description:
+ */
+const { override, fixBabelImports } = require('customize-cra')
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css'
+  })
+)
